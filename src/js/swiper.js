a2869d6a828b1e1cad2e150657977cba589cf061
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-06-10 16:12:19 +0300

    add projects

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,11 +1,17 @@
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 
-const goodsSlider = new Swiper('.goods-slider', {
+const projectsSlider = new Swiper('.projects-slider', {
+  modules: [Navigation],
   direction: 'horizontal',
   spaceBetween: -23,
   speed: 800,
   loop: true,
   grabCursor: true,
   slidesPerView: 'auto',
+  navigation: {
+    nextEl: '.projects-slider-btn-next',
+    prevEl: '.projects-slider-btn-prev',
+  },
 });
