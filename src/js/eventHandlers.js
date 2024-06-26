7fc11fc895524d284adda9d4b6418f0917b5679b
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-06-26 12:40:52 +0300

    fix modalWin script

--- src/js/eventHandlers.js
+++ src/js/eventHandlers.js
@@ -1,132 +1,16 @@
 import refs from './refs';
 
-refs.projectArmorBtn.forEach((btn) => {
-  btn.addEventListener('click', onProjectArmorBtnClick);
+const isHiddenClassName = 'is-hidden';
+
+refs.projectsLists.forEach((list) => {
+  list.addEventListener('click', onProjectBtnClick);
 });
-refs.projectArtemsilBtn.forEach((btn) => {
-  btn.addEventListener('click', onProjectArtemsilBtnClick);
+refs.closeBtn.forEach((btn) => {
+  btn.addEventListener('click', onCloseBtnClick);
 });
-refs.projectMoneyBtn.forEach((btn) => {
-  btn.addEventListener('click', onProjectMoneyBtnClick);
-});
-refs.projectMessageBtn.forEach((btn) => {
-  btn.addEventListener('click', onProjectMessageBtnClick);
-});
-refs.armorModalWinCloseBtn.addEventListener('click', onProjectArmorBtnClick);
-refs.artemsilModalWinCloseBtn.addEventListener(
-  'click',
-  onProjectArtemsilBtnClick
-);
-refs.moneyModalWinCloseBtn.addEventListener('click', onProjectMoneyBtnClick);
-refs.messageModalWinCloseBtn.addEventListener(
-  'click',
-  onProjectMessageBtnClick
-);
 refs.backdrop.forEach((item) => {
   item.addEventListener('click', onBackdropClick);
 });
-
-const isHiddenClassName = 'is-hidden';
-
-function onProjectArmorBtnClick(e) {
-  e.currentTarget.blur();
-  toggleShowArmorModalWin();
-
-  const isHidden =
-    refs.projectArmorModalWin.classList.contains(isHiddenClassName);
-
-  if (isHidden) {
-    window.removeEventListener('keydown', hideProjectArmorModalWin);
-  } else {
-    window.addEventListener('keydown', hideProjectArmorModalWin);
-  }
-}
-
-function hideProjectArmorModalWin(e) {
-  if (e.code === 'Escape') {
-    toggleShowArmorModalWin();
-    window.removeEventListener('keydown', hideProjectArmorModalWin);
-  }
-}
-
-function toggleShowArmorModalWin() {
-  refs.projectArmorModalWin.classList.toggle(isHiddenClassName);
-}
-
-function onProjectArtemsilBtnClick(e) {
-  e.currentTarget.blur();
-  toggleShowArtemsilModalWin();
-
-  const isHidden =
-    refs.projectArtemsilModalWin.classList.contains(isHiddenClassName);
-
-  if (isHidden) {
-    window.removeEventListener('keydown', hideProjectArtemsilModalWin);
-  } else {
-    window.addEventListener('keydown', hideProjectArtemsilModalWin);
-  }
-}
-
-function hideProjectArtemsilModalWin(e) {
-  if (e.code === 'Escape') {
-    toggleShowArtemsilModalWin();
-    window.removeEventListener('keydown', hideProjectArtemsilModalWin);
-  }
-}
-
-function toggleShowArtemsilModalWin() {
-  refs.projectArtemsilModalWin.classList.toggle(isHiddenClassName);
-}
-
-function onProjectMoneyBtnClick(e) {
-  e.currentTarget.blur();
-  toggleShowMoneyModalWin();
-
-  const isHidden =
-    refs.projectMoneyModalWin.classList.contains(isHiddenClassName);
-
-  if (isHidden) {
-    window.removeEventListener('keydown', hideProjectMoneyModalWin);
-  } else {
-    window.addEventListener('keydown', hideProjectMoneyModalWin);
-  }
-}
-
-function hideProjectMoneyModalWin(e) {
-  if (e.code === 'Escape') {
-    toggleShowMoneyModalWin();
-    window.removeEventListener('keydown', hideProjectMoneyModalWin);
-  }
-}
-
-function toggleShowMoneyModalWin() {
-  refs.projectMoneyModalWin.classList.toggle(isHiddenClassName);
-}
-
-function onProjectMessageBtnClick(e) {
-  e.currentTarget.blur();
-  toggleShowMessageModalWin();
-
-  const isHidden =
-    refs.projectMessageModalWin.classList.contains(isHiddenClassName);
-
-  if (isHidden) {
-    window.removeEventListener('keydown', hideProjectMessageModalWin);
-  } else {
-    window.addEventListener('keydown', hideProjectMessageModalWin);
-  }
-}
-
-function hideProjectMessageModalWin(e) {
-  if (e.code === 'Escape') {
-    toggleShowMessageModalWin();
-    window.removeEventListener('keydown', hideProjectMessageModalWin);
-  }
-}
-
-function toggleShowMessageModalWin() {
-  refs.projectMessageModalWin.classList.toggle(isHiddenClassName);
-}
 
 function onBackdropClick(e) {
   if (e.currentTarget !== e.target) {
@@ -134,8 +18,44 @@ function onBackdropClick(e) {
   }
 
   e.currentTarget.parentNode.classList.add(isHiddenClassName);
-  window.removeEventListener('keydown', hideProjectArmorModalWin);
-  window.removeEventListener('keydown', hideProjectArtemsilModalWin);
-  window.removeEventListener('keydown', hideProjectMoneyModalWin);
-  window.removeEventListener('keydown', hideProjectMessageModalWin);
+  window.removeEventListener('keydown', hideProjectModalWin);
+}
+
+function onCloseBtnClick(e) {
+  const targetModalWin = e.target.closest('.js-project-modal-win');
+  targetModalWin.classList.add(isHiddenClassName);
+}
+
+function onProjectBtnClick(e) {
+  const targetBtn = e.target.closest('.js-project-btn');
+
+  if (!targetBtn) {
+    return;
+  }
+  targetBtn.blur();
+  const { project } = targetBtn.dataset;
+  const targetProjectModalWin = document.querySelector(
+    `.js-project-modal-win.${project}`
+  );
+
+  targetProjectModalWin.classList.remove(isHiddenClassName);
+
+  const isHidden = targetProjectModalWin.classList.contains(isHiddenClassName);
+
+  if (isHidden) {
+    window.removeEventListener('keydown', hideProjectModalWin);
+  } else {
+    window.addEventListener('keydown', hideProjectModalWin);
+  }
+}
+
+function hideProjectModalWin(e) {
+  if (e.code === 'Escape') {
+    const targetProjectModalWin = document.querySelector(
+      `.js-project-modal-win:not(.${isHiddenClassName})`
+    );
+
+    targetProjectModalWin.classList.add(isHiddenClassName);
+    window.removeEventListener('keydown', hideProjectModalWin);
+  }
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,30 +1,9 @@
 const refs = {
-  projectArmorBtn: document.querySelectorAll('.js-project-armor-btn'),
-  projectArtemsilBtn: document.querySelectorAll('.js-project-artemsil-btn'),
-  projectMoneyBtn: document.querySelectorAll('.js-project-money-btn'),
-  projectMessageBtn: document.querySelectorAll('.js-project-message-btn'),
-  projectArmorModalWin: document.querySelector('.js-project-armor-modal-win'),
-  projectArtemsilModalWin: document.querySelector(
-    '.js-project-artemsil-modal-win'
-  ),
-  projectMoneyModalWin: document.querySelector('.js-project-money-modal-win'),
-  projectMessageModalWin: document.querySelector(
-    '.js-project-message-modal-win'
-  ),
-  armorModalWinCloseBtn: document.querySelector(
-    '.js-armor-modal-win-close-btn'
-  ),
-  artemsilModalWinCloseBtn: document.querySelector(
-    '.js-artemsil-modal-win-close-btn'
-  ),
-  moneyModalWinCloseBtn: document.querySelector(
-    '.js-money-modal-win-close-btn'
-  ),
-  messageModalWinCloseBtn: document.querySelector(
-    '.js-message-modal-win-close-btn'
-  ),
+  projectsLists: document.querySelectorAll('.js-projects-list'),
+  closeBtn: document.querySelectorAll('.modal-win-close-btn'),
   backdrop: document.querySelectorAll('.js-backdrop'),
   backdropWrap: document.querySelectorAll('.js-backdrop-wrap'),
+  mainNavigation: document.querySelectorAll('.js-main-navigation'),
 };
 
 export default refs;
